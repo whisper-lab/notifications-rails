cf1ab6cb83b6c4e5fbbaaa9b7db93c1aa9849565
Alexey Pustobaev <L@whisperlab.com>
2015-02-16 12:21:46 +0300

    tempfile fix

--- app/workers/ios_alert_worker.rb
+++ app/workers/ios_alert_worker.rb
@@ -16,48 +16,52 @@ class IosAlertWorker
 
   def perform
     fname = "tempfile.pem"
-    # open the file, and copy the contents from the file on AWS-S3
-    File.open(fname, 'wb') do |fo|
-      # fo.print open(Aws::S3::S3Object.url_for('certificate.pem', S3_BUCKET_PNS)).read
-      fo.print S3_BUCKET_PNS.object('certificate.pem').get.body.read
-    end
-    file = File.new(fname)
+    temp_file = Tempfile.new(fname)
+    begin
+      # open the file, and copy the contents from the file on AWS-S3
+      File.open(temp_file, 'wb') do |fo|
+        # fo.print open(Aws::S3::S3Object.url_for('certificate.pem', S3_BUCKET_PNS)).read
+        fo.print S3_BUCKET_PNS.object('certificate.pem').get.body.read
+      end
+      file = File.new(temp_file)
 
-    # Keep the connection to gateway.push.apple.com open throughout the duration of this job
-    pusher = Grocer.pusher(
-      certificate: file.path,                # required
-      passphrase:  "",                       # optional
-      gateway:     "gateway.sandbox.push.apple.com", # optional
-      port:        2195,                     # optional
-      retries:     3                         # optional
-    )
-
-    User.joins(:device).where('devices.platform = ?', 'ios').pluck('devices.token').each do |token|
-      notification = Grocer::Notification.new(
-        device_token:      token,
-        alert:             "Hello from Grocer!",
-        badge:             42,
-        sound:             "siren.aiff",         # optional
-        expiry:            Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
-        identifier:        1234,                 # optional
-        content_available: true                  # optional; any truthy value will set 'content-available' to 1
+      # Keep the connection to gateway.push.apple.com open throughout the duration of this job
+      pusher = Grocer.pusher(
+        certificate: file.path,                # required
+        passphrase:  "",                       # optional
+        gateway:     "gateway.sandbox.push.apple.com", # optional
+        port:        2195,                     # optional
+        retries:     3                         # optional
       )
-      pusher.push(notification)
+
+      User.joins(:device).where('devices.platform = ?', 'ios').pluck('devices.token').each do |token|
+        notification = Grocer::Notification.new(
+          device_token:      token,
+          alert:             "Hello from Grocer!",
+          badge:             42,
+          sound:             "siren.aiff",         # optional
+          expiry:            Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
+          identifier:        1234,                 # optional
+          content_available: true                  # optional; any truthy value will set 'content-available' to 1
+        )
+        pusher.push(notification)
+      end
+
+      feedback = Grocer.feedback(
+          certificate: file.path,                 # required
+          passphrase:  "",                        # optional
+          gateway:     "feedback.push.apple.com", # optional; See note below.
+          port:        2196,                      # optional
+          retries:     3                          # optional
+      )
+
+      feedback.each do |attempt|
+        puts "Device #{attempt.device_token} failed at #{attempt.timestamp}"
+      end
+    ensure
+      temp_file.close
+      # delete the temporary file
+      File.delete(temp_file)
     end
-
-    feedback = Grocer.feedback(
-        certificate: file.path,                 # required
-        passphrase:  "",                        # optional
-        gateway:     "feedback.push.apple.com", # optional; See note below.
-        port:        2196,                      # optional
-        retries:     3                          # optional
-    )
-
-    feedback.each do |attempt|
-      puts "Device #{attempt.device_token} failed at #{attempt.timestamp}"
-    end
-
-    # delete the temporary file
-    File.delete(fname)
   end
 end
