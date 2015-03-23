8dca4542e716b9a4a8d7fae75515ec716a5ddd72
Alexey Pustobaev <L@whisperlab.com>
2015-03-23 13:33:47 +0300

    body not nil

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,19 @@
 class Message < ActiveRecord::Base
   before_create :set_date_to_now
+  before_create :set_body_to_empty_string
 
   belongs_to :channel
 
   validates :channel_id, presence: true
+  validates :body, presence: true
 
   private
 
     def set_date_to_now
       self.date ||= DateTime.now
+    end
+
+    def set_body_to_empty_string
+      self.body ||= ""
     end
 end
