8183d734d075cd9793c4286bc6f448ba2793e52f
Alexey Pustobaev <L@whisperlab.com>
2015-03-23 14:33:04 +0300

    title fix, pre tag

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,10 @@
 module MessagesHelper
+
+  def get_formatted_title(message)
+    if (message.title.nil? || message.title.empty?)
+      title = raw(message.channel.name)
+    else
+      title = raw(message.title)
+    end
+  end
 end
